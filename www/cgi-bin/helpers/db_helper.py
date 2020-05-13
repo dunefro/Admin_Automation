4b92c5c9d3f20ffc48d4d9556425272fae729da9
Vedant Pareek <dunefro@gmail.com>
2020-05-14 00:01:17 +0530

    Added redis

--- www/cgi-bin/helpers/db_helper.py
+++ www/cgi-bin/helpers/db_helper.py
@@ -5,5 +5,7 @@ logging.basicConfig(level=logging.INFO)
 client = docker.from_env()
 
 def create_db(db_name):
+    if db_name == 'redis':
+        client.containers.run(image='redis:2.6-32bit')
     logging.info(db_name)
     return True
