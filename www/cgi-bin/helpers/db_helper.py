462f4317178da99fdd87288e4dd5100c0de92b9b
Vedant Pareek <dunefro@gmail.com>
2020-05-13 23:43:06 +0530

    Initial commit

--- www/cgi-bin/helpers/db_helper.py
+++ www/cgi-bin/helpers/db_helper.py
@@ -1,5 +1,9 @@
 import docker
+import logging
 
+logging.basicConfig(level=logging.INFO)
 client = docker.from_env()
 
-def create_db()
+def create_db(db_name):
+    logging.info(db_name)
+    return True
