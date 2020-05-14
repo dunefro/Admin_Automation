028fb1dd7309ef366119a52e811a051a7ac64567
Vedant Pareek <dunefro@gmail.com>
2020-05-14 21:29:00 +0530

    Added mysql empty password

--- www/cgi-bin/helpers/db_helper.py
+++ www/cgi-bin/helpers/db_helper.py
@@ -5,12 +5,13 @@ logging.basicConfig(level=logging.INFO)
 client = docker.from_env()
 
 def create_db(db_name):
+    logging.info('Received databse {}'.format(db_name))
     if db_name == 'redis':
         client.containers.run(image='redis:latest',detach=True)
     elif db_name == 'es':
         client.containers.run(image='elasticsearch:latest', detach=True)
     elif db_name == 'mysql':
-        client.containers.run(image='mysql:latest', detach=True)
+        client.containers.run(image='mysql:latest', detach=True,environment=["MYSQL_ALLOW_EMPTY_PASSWORD=True"])
     elif db_name == 'mongodb':
         client.containers.run(image='mongo:latest', detach=True)
     else:
