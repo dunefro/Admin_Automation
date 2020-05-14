a2409b93758e6080cfbbdd90643d5cc148552eca
Vedant Pareek <dunefro@gmail.com>
2020-05-14 23:24:39 +0530

    Added space for more client

--- www/cgi-bin/helpers/mail_helper.py
+++ www/cgi-bin/helpers/mail_helper.py
@@ -10,3 +10,4 @@ def send_mail(client_name):
     if client_name == "one":
         client.containers.run(image='dunefro/mail:v3',environment=["MAIL=pareekvedant99@gmail.com"],detach=True)
         logging.info("Mail sent check the container logs or your inbox")
+    # Space for adding more clients
