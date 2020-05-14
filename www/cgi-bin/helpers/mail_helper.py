1d4436b5ac83eee171ad765f945dfaea8e840473
Vedant Pareek <dunefro@gmail.com>
2020-05-14 23:23:57 +0530

    Customized mail helper

--- www/cgi-bin/helpers/mail_helper.py
+++ www/cgi-bin/helpers/mail_helper.py
@@ -1,6 +1,12 @@
 import docker
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 client = docker.from_env()
 
-def send_mail(mail=None, dunefro=None):
-    client.containers.run(image='dunefro/mail')
+def send_mail(client_name):
+    logging.info('Client mail request received {}'.format(client_name))
+    if client_name == "one":
+        client.containers.run(image='dunefro/mail:v3',environment=["MAIL=pareekvedant99@gmail.com"],detach=True)
+        logging.info("Mail sent check the container logs or your inbox")
