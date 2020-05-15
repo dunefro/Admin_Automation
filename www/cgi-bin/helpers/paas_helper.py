f9d0ddb5db2e9f793424d968d90c0c6b47b7774b
Vedant Pareek <dunefro@gmail.com>
2020-05-15 22:24:15 +0530

    Using check_port outside the file scope

--- www/cgi-bin/helpers/paas_helper.py
+++ www/cgi-bin/helpers/paas_helper.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 client = docker.from_env()
 
-def _check_port(port_no):
+def check_port(port_no):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     res = sock.connect_ex(('127.0.0.1', port_no))
@@ -16,7 +16,7 @@ def _check_port(port_no):
 def create_python_shell():
     while True:
         paas_port = randint(49152,65535)
-        port_list = [port for port in range(49512,65536) if _check_port(port) == 0 ]
+        port_list = [port for port in range(49512,65536) if check_port(port) == 0 ]
         if paas_port not in port_list:
             logging.info(paas_port)
             client.containers.run('dunefro/pass_shell:v1', detach=True,tty=True,ports={'4200/tcp': paas_port})
